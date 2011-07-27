adf3061ced3619438b21755f6db15771336fd966
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2011-07-27 14:10:26 +0100

    Updated version to 0.10.00

--- maidsafe_common_lib/src/maidsafe/common/platform_config.h
+++ maidsafe_common_lib/src/maidsafe/common/platform_config.h
@@ -71,7 +71,7 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #endif
 #include "maidsafe/common/version.h"
 
-#if MAIDSAFE_COMMON_VERSION != 903
+#if MAIDSAFE_COMMON_VERSION != 1000
 #  error This API is not compatible with the installed library.\
     Please update the MaidSafe-Common library.
 #endif
