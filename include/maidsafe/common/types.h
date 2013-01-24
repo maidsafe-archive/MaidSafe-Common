dc8236a4524906ff30ac798b3e436b4f748fcffc
Brian Smith <brian.smith@maidsafe.net>
2013-01-24 10:16:56 +0000

    Removed unneeded type definition.

--- include/maidsafe/common/types.h
+++ include/maidsafe/common/types.h
@@ -45,8 +45,6 @@ typedef NonEmptyString UserPassword;
 typedef TaggedValue<uint64_t, struct MemoryUsageTag> MemoryUsage;
 typedef TaggedValue<uint64_t, struct DiskUsageTag> DiskUsage;
 
-typedef TaggedValue<uint64_t, struct CacheSizeTag> CacheSize;
-
 template<typename T>
 struct is_long_term_cacheable : public std::false_type {};
 
