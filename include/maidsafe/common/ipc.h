6e5cfb11498fffe09c7c976f8401922494edd5db
David Irvine <david.irvine@maidsafe.net>
2013-10-22 22:44:34 +0100

    Added a simplified IPC for passing a single type to a running process

---
+++ include/maidsafe/common/ipc.h
@@ -0,0 +1,64 @@
+/*  Copyright 2012 MaidSafe.net limited
+
+    This MaidSafe Software is licensed to you under (1) the MaidSafe.net Commercial License,
+    version 1.0 or later, or (2) The General Public License (GPL), version 3, depending on which
+    licence you accepted on initial access to the Software (the "Licences").
+
+    By contributing code to the MaidSafe Software, or to this project generally, you agree to be
+    bound by the terms of the MaidSafe Contributor Agreement, version 1.0, found in the root
+    directory of this project at LICENSE, COPYING and CONTRIBUTOR respectively and also
+    available at: http://www.maidsafe.net/licenses
+
+    Unless required by applicable law or agreed to in writing, the MaidSafe Software distributed
+    under the GPL Licence is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+    OF ANY KIND, either express or implied.
+
+    See the Licences for the specific language governing permissions and limitations relating to
+    use of the MaidSafe Software.                                                                 */
+
+#ifndef MAIDSAFE_COMMON_IPC_H_
+#define MAIDSAFE_COMMON_IPC_H_
+
+#include <boost/interprocess/managed_shared_memory.hpp>
+#include <string>
+
+namespace maidsafe {
+namespace ipc {
+
+namespace bi = boost::interprocess;
+
+// NOTE!, dereferencing pointers in containers is not allowed in these functions as is
+// therefor no stl containers or types that require dereferencing will work
+// unless bi::offset_ptr is used to create the pointer types in containers
+// or classes. No VECTORS, MAP or SETS etc. please roll your own in that case from
+// http://www.boost.org/doc/libs/Release/doc/html/index.html
+
+// This is an extreme simplification of boost::ipc to allow simple types can be passed.
+// there is no mem trimming at all, we always use the full 65536 bytes
+// there is also only the ability to have one item in the SHM
+// increasing number of items is trivial if required and is done by naming
+// each instance (where now we have "a" declared) and adding several
+// items to the SHM. "a" is used here to make mem surfing a little harder
+
+template <typename Type>
+void CreateSharedMem(std::string name, Type type) {
+  bi::shared_memory_object::remove(name.c_str());
+  //Create a managed shared memory segment of large arbitary size !
+  bi::managed_shared_memory segment(bi::create_only, name.c_str(), 65536);
+  //Create an object of Type initialized to type
+  segment.construct<Type>("a") (type);
+}
+
+template <typename Type>
+auto ReadSharedMem(std::string name)->Type {
+  // Open managed segment
+  bi::managed_shared_memory segment(bi::open_only, name.c_str());
+  return *bi::offset_ptr<Type>(segment.find<Type>("a").first);
+}
+
+void RemoveSharedMem(std::string name);
+
+}  // namespace ipc
+}  // namespace maidsafe
+
+#endif  // MAIDSAFE_COMMON_IPC_H_
---
+++ src/maidsafe/common/tests/ipc_test.cc
@@ -0,0 +1,65 @@
+/*  Copyright 2009 MaidSafe.net limited
+
+    This MaidSafe Software is licensed to you under (1) the MaidSafe.net Commercial License,
+    version 1.0 or later, or (2) The General Public License (GPL), version 3, depending on which
+    licence you accepted on initial access to the Software (the "Licences").
+
+    By contributing code to the MaidSafe Software, or to this project generally, you agree to be
+    bound by the terms of the MaidSafe Contributor Agreement, version 1.0, found in the root
+    directory of this project at LICENSE, COPYING and CONTRIBUTOR respectively and also
+    available at: http://www.maidsafe.net/licenses
+
+    Unless required by applicable law or agreed to in writing, the MaidSafe Software distributed
+    under the GPL Licence is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+    OF ANY KIND, either express or implied.
+
+    See the Licences for the specific language governing permissions and limitations relating to
+    use of the MaidSafe Software.                                                                 */
+
+#include "maidsafe/common/ipc.h"
+
+#include <string>
+#include <vector>
+
+#include "maidsafe/common/test.h"
+
+namespace maidsafe {
+
+namespace test {
+
+// when run in ctest these tests will be seperate processes. We must run them in sequence
+
+TEST_CASE("ipc create", "[ipc][Unit]") {
+  int int_val(123);
+  std::string str("test ipc");
+  struct Simple {
+    int a = 1;
+    std::string str = "a long test string";
+  } simple;
+  CHECK_NOTHROW(ipc::CreateSharedMem<Simple>("struct_test", simple));
+  CHECK_NOTHROW(ipc::CreateSharedMem<int>("int_test", int_val));
+  CHECK_NOTHROW(ipc::CreateSharedMem<std::string>("str_test", str));
+}
+
+TEST_CASE("ipc read", "[ipc][Unit]") {
+  int int_val_orig(123);
+  std::string str_orig("test ipc");
+  struct Simple {
+    int a{1};
+    std::string str{"a long test string"};
+  } simple_orig;
+  CHECK(simple_orig.a == ipc::ReadSharedMem<Simple>(std::string("struct_test")).a);
+  CHECK(simple_orig.str == ipc::ReadSharedMem<Simple>(std::string("struct_test")).str);
+  CHECK(int_val_orig == ipc::ReadSharedMem<int>(std::string("int_test")));
+  CHECK(str_orig == ipc::ReadSharedMem<std::string>(std::string("str_test")));
+}
+
+TEST_CASE("ipc delete", "[ipc][Unit]") {
+  // always passes, even if SHM noexists
+  CHECK_NOTHROW(ipc::RemoveSharedMem("vec_test"));
+  CHECK_NOTHROW(ipc::RemoveSharedMem("str_test"));
+}
+
+}  // namespace test
+
+}  // namespace maidsafe
