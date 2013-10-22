04ca070fb985f880310f00970e017cdcf3689eb2
David Irvine <david.irvine@maidsafe.net>
2013-10-22 23:47:02 +0100

    style fixes

--- include/maidsafe/common/ipc.h
+++ include/maidsafe/common/ipc.h
@@ -41,16 +41,13 @@ namespace bi = boost::interprocess;
 // each instance (where now we have "a" declared) and adding several
 // items to the SHM. "a" is used here to make mem surfing a little harder
 
-// these must be used as string types std::string WILL FAIL
-typedef bi::allocator<char, boost::interprocess::managed_shared_memory::segment_manager> CharAllocator;
-typedef bi::basic_string<char, std::char_traits<char>, CharAllocator> string;
 
 template <typename Type>
 void CreateSharedMem(std::string name, Type type) {
   bi::shared_memory_object::remove(name.c_str());
-  //Create a managed shared memory segment of large arbitary size !
+    //Create a managed shared memory segment of large arbitary size !
   bi::managed_shared_memory segment(bi::create_only, name.c_str(), 65536);
-  //Create an object of Type initialized to type
+    //Create an object of Type initialized to type
   segment.construct<Type>("a") (type);
 }
 
