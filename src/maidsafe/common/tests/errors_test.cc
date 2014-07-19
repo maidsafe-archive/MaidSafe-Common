2761874c59930e1095db0d75f9616a3560b63e2c
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2014-07-19 04:49:30 +0100

    Bugfix for Release builds with Clang.

--- src/maidsafe/common/tests/errors_test.cc
+++ src/maidsafe/common/tests/errors_test.cc
@@ -180,8 +180,7 @@ TYPED_TEST_CASE(MaidSafeErrorTest, AllErrorTypes);
 
 TYPED_TEST(MaidSafeErrorTest, BEH_ConstructorsAndHelpers) {
   const std::error_code kCode{ make_error_code(typename EnumClass<TypeParam>::type(1)) };
-  const std::error_condition kCondition{
-      make_error_condition(typename EnumClass<TypeParam>::type(1)) };
+  make_error_condition(typename EnumClass<TypeParam>::type(1));
   const std::string kWhat{ RandomAlphaNumericString(10) };
 
   TypeParam error(kCode, kWhat);
