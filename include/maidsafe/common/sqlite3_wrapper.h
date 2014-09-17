b5121e4d468293c7becbdd484ac546d623cf1536
Qi Ma <qi.ma@maidsafe.net>
2014-09-17 10:40:10 +0100

    sqlite wrapper minor API change

--- include/maidsafe/common/sqlite3_wrapper.h
+++ include/maidsafe/common/sqlite3_wrapper.h
@@ -47,8 +47,6 @@ struct Database {
   Database(Database&&) = delete;
   Database& operator=(Database) = delete;
 
-  void CheckPoint();
-
   friend struct Tranasction;
   friend struct Statement;
  private:
