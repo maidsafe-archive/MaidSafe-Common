d246c6172953e5cdee1190289ebaea840ecb4958
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2014-09-27 12:23:01 +0100

    Updated address_space_tool.

--- src/maidsafe/common/tools/address_space_tool.h
+++ src/maidsafe/common/tools/address_space_tool.h
@@ -38,6 +38,8 @@
 #include <utility>
 #include <vector>
 
+#include "cereal/cereal.hpp"
+
 #include "maidsafe/common/config.h"
 #include "maidsafe/common/node_id.h"
 
@@ -58,25 +60,25 @@ namespace tools {
 // CandidiateCLB < GroupCLB + 'config.leeway'.
 enum class CommonLeadingBitsAlgorithm {
   kClosest,  // CandidiateCLB == CLB between candidate and closest in group.
-  // GroupCLB == CLB between closest and second closest in group.
+             // GroupCLB == CLB between closest and second closest in group.
   kHighest,  // CandidiateCLB == highest CLB between candidate and each node in close group.
-  // GroupCLB == highest CLB between any two nodes in close group.
-  kLowest,  // CandidiateCLB == lowest CLB between candidate and each node in close group.
-  // GroupCLB == lowest CLB between any two nodes in close group
-  kMean,  // CandidateCLB == mean CLB rounded down to nearest int between candidate and each node
-  // in close group.
-  // GroupCLB == mean CLB rounded down to nearest int between all nodes in close group.
-  kNone  // CLBs aren't considered when adding a new node.
+             // GroupCLB == highest CLB between any two nodes in close group.
+  kLowest,   // CandidiateCLB == lowest CLB between candidate and each node in close group.
+             // GroupCLB == lowest CLB between any two nodes in close group
+  kMean,     // CandidateCLB == mean CLB rounded down to nearest int between candidate and each node
+             // in close group.
+             // GroupCLB == mean CLB rounded down to nearest int between all nodes in close group.
+  kNone      // CLBs aren't considered when adding a new node.
 };
 
 struct Config {
-  size_t iterations{5};                   // Test iterations
+  size_t iterations{4};                   // Test iterations
   size_t initial_good_count{1000};        // No. of good nodes used to pre-populate network on first
                                           // test iteration
   double initial_factor{2.0};             // Factor to multiply 'initial_good_count' by for each
                                           // test iteration
-  size_t group_size{4};                   // Close group size
-  size_t majority_size{3};                // Majority of close group required to make a mutating
+  size_t group_size{11};                  // Close group size
+  size_t majority_size{7};                // Majority of close group required to make a mutating
                                           // action on data
   size_t bad_group_count{1};              // Target no. of "bad groups" (i.e. a group where
                                           // 'majority_size' are bad)
@@ -87,7 +89,76 @@ struct Config {
                                           // allowed to be compared to the other nodes
   size_t good_added_per_bad{0};           // No. of good nodes added every time a bad node is added
   CommonLeadingBitsAlgorithm algorithm{CommonLeadingBitsAlgorithm::kLowest};  // Described above
+
+  template <typename Archive>
+  void save(Archive& archive) const {
+    archive(CEREAL_NVP(iterations), CEREAL_NVP(initial_good_count), CEREAL_NVP(initial_factor),
+            CEREAL_NVP(group_size), CEREAL_NVP(majority_size), CEREAL_NVP(bad_group_count),
+            CEREAL_NVP(total_random_attempts), CEREAL_NVP(leeway), CEREAL_NVP(good_added_per_bad),
+            CEREAL_NVP(algorithm));
+  }
+
+  template <typename Archive, typename NameValuePair>
+  void load_optional_element(Archive& archive, NameValuePair name_value_pair) {
+    try {
+      archive(name_value_pair);
+    }
+    catch (const std::exception& e) {
+      std::string g = e.what();
+      (void)g;
+    }
+  }
+
+  template <typename Archive>
+  void load(Archive& archive) {
+    load_optional_element(archive, CEREAL_NVP(iterations));
+    load_optional_element(archive, CEREAL_NVP(initial_good_count));
+    load_optional_element(archive, CEREAL_NVP(initial_factor));
+    load_optional_element(archive, CEREAL_NVP(group_size));
+    load_optional_element(archive, CEREAL_NVP(majority_size));
+    load_optional_element(archive, CEREAL_NVP(bad_group_count));
+    load_optional_element(archive, CEREAL_NVP(total_random_attempts));
+    load_optional_element(archive, CEREAL_NVP(leeway));
+    load_optional_element(archive, CEREAL_NVP(good_added_per_bad));
+    load_optional_element(archive, CEREAL_NVP(algorithm));
+  }
 };
+
+template <typename Elem, typename Traits>
+std::basic_ostream<Elem, Traits>& operator<<(std::basic_ostream<Elem, Traits>& ostream,
+                                             const Config& config) {
+  ostream << "\titerations:            " << config.iterations << '\n';
+  ostream << "\tinitial_good_count:    " << config.initial_good_count << '\n';
+  ostream << "\tinitial_factor:        " << config.initial_factor << '\n';
+  ostream << "\tgroup_size:            " << config.group_size << '\n';
+  ostream << "\tmajority_size:         " << config.majority_size << '\n';
+  ostream << "\tbad_group_count:       " << config.bad_group_count << '\n';
+  ostream << "\ttotal_random_attempts: " << config.total_random_attempts << '\n';
+  ostream << "\tleeway:                " << config.leeway << '\n';
+  ostream << "\tgood_added_per_bad:    " << config.good_added_per_bad << '\n';
+  ostream << "\talgorithm:             ";
+  switch (config.algorithm) {
+    case CommonLeadingBitsAlgorithm::kClosest:
+      ostream << "kClosest";
+      break;
+    case CommonLeadingBitsAlgorithm::kHighest:
+      ostream << "kHighest";
+      break;
+    case CommonLeadingBitsAlgorithm::kLowest:
+      ostream << "kLowest";
+      break;
+    case CommonLeadingBitsAlgorithm::kMean:
+      ostream << "kMean";
+      break;
+    case CommonLeadingBitsAlgorithm::kNone:
+      ostream << "kNone";
+      break;
+    default:
+      ostream << "INVALID VALUE";
+  }
+  ostream << '\n';
+  return ostream;
+}
 
 
 
