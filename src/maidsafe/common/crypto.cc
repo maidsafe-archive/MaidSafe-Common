a8eeb46b0818f8284b5d8cf239f046c0529b99f9
David Irvine <david.irvine@maidsafe.net>
2014-12-08 16:05:56 +0000

    Added assert for sqlite3 multi threaded and initial tests, jira test added for multi process tests

--- src/maidsafe/common/crypto.cc
+++ src/maidsafe/common/crypto.cc
@@ -256,7 +256,51 @@ std::string InfoRetrieve(int32_t threshold, const std::vector<std::string>& in_s
         recovery, std::string(reinterpret_cast<char*>(channel.begin()), 4)));
   }
 
+>>>>>>> Stashed changes
+  while (string_sources[0]->Pump(256)) {
+    for (auto i = 1; i < num_to_check; ++i)
+      string_sources[i]->Pump(256);
+  }
+  source.PumpAll();
+  return out_strings;
+}
+
+<<<<<<< Updated upstream
+=======
   for (int32_t i = 0; i < threshold; ++i)
+    string_sources[i]->PumpAll();
+
+  return data;
+}
+
+std::vector<byte> InfoRetrieve(int32_t threshold, const std::vector<std::vector<byte>>& in_bytes) {
+  int32_t size(static_cast<int32_t>(in_bytes.size()));
+  int32_t num_to_check = std::min(size, threshold);
+  // Safe to subtract 4 since each piece is prefixed with a byte piece number
+  auto data_size(num_to_check * (in_bytes.front().size() - 4));
+  std::vector<byte> data(data_size);
+
+  auto array_sink = new CryptoPP::ArraySink(&data.data()[0], data_size);
+  CryptoPP::InformationRecovery recovery(num_to_check, array_sink);
+  CryptoPP::vector_member_ptrs<CryptoPP::ArraySource> array_sources(num_to_check);
+  CryptoPP::SecByteBlock channel(4);
+
+  for (auto i = 0; i < num_to_check; ++i) {
+    array_sources[i].reset(
+        new CryptoPP::ArraySource(&in_bytes[i].data()[0], in_bytes[i].size(), false));
+    array_sources[i]->Pump(4);
+    array_sources[i]->Get(channel, 4);
+    array_sources[i]->Attach(new CryptoPP::ChannelSwitch(
+        recovery, std::string(reinterpret_cast<char*>(channel.begin()), 4)));
+  }
+
+  while (array_sources[0]->Pump(256)) {
+    for (auto i = 1; i < num_to_check; ++i)
+      array_sources[i]->Pump(256);
+  }
+
+>>>>>>> Stashed changes
+  for (auto i = 0; i < num_to_check; ++i)
     string_sources[i]->PumpAll();
 
   return data;
