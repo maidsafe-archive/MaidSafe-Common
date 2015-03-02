5351dad48fa50d35eeadaa181ecf08592340e056
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spam@nowhere>
2015-03-02 18:09:33 +0000

    Make unused parameters and maybe uninitialised warnings no longer error.

--- common_flags.cmake
+++ common_flags.cmake
@@ -157,6 +157,7 @@ target_compile_options(maidsafe_common
         -Wredundant-decls
         -Wold-style-cast
         -Woverloaded-virtual
+        -Wno-error=unused-parameter     # Having this error creates grief for those using unused parameters during debugging sessions
         -fPIC
         -fstrict-overflow
         -pedantic
@@ -179,6 +180,10 @@ target_compile_options(maidsafe_common
                 -Wno-unused-command-line-argument
             >
         >
-        $<$<CXX_COMPILER_ID:GNU>:-static-libstdc++ -Wstrict-null-sentinel>
+        $<$<CXX_COMPILER_ID:GNU>:
+            -static-libstdc++
+            -Wstrict-null-sentinel
+            -Wno-error=maybe-uninitialized  # GCC's heuristics for this warning are known to generate a lot of false positives
+        >
     >
 )
